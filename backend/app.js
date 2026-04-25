e20fe9c5a57d567d77303967998cccd49948b238
Yoni190 <yfisuhu@gmail.com>
2026-04-25 15:29:39 +0300

    Allow json body

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,12 @@
 const express = require('express')
-const { prisma } = require('./lib/prisma.js')
+const characterRoutes = require('./routes/characterRoute.js')
 
 require('dotenv').config()
 
 const app = express()
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-    const characters = await prisma.character.findMany()
-    res.json({ message: characters })
-})
+app.use('/characters', characterRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, (error) => {
