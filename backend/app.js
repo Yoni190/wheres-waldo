e17e2f6797093815dd0bd533f8f6e769dcbe1ad6
Yoni190 <yfisuhu@gmail.com>
2026-04-25 15:36:01 +0300

    Add cors package

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const characterRoutes = require('./routes/characterRoute.js')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 app.use('/characters', characterRoutes)
 
